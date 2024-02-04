85f3441ae78f2a62d899d949df144139be795b89
1xtAsh <ashraykrsingh321@gmail.com>
2024-02-04 13:47:11 +0530

    sm8350-common: Drop some more prebuilt packages

--- removepackagesgms/Android.mk
+++ removepackagesgms/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackagesGms
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES += Maps FilesPrebuilt talkback PrebuiltGmail ScribePrebuilt TouchGestures
+LOCAL_OVERRIDES_PACKAGES += Maps FilesPrebuilt talkback PrebuiltGmail ScribePrebuilt TouchGestures DotGallery ViaBrowser DuckDuckGo
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
